aaec7708ca915f70697b28f3c7cc8831421bbe52
Miguel Ángel Navas García <manavas@gmail.com>
2019-10-04 15:57:13 +0200

    Primo mas ignore

---
+++ src/main/java/es/navas/oposiciones/NumeroPrimo.java
@@ -0,0 +1,20 @@
+package es.navas.oposiciones;
+
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
+public class NumeroPrimo {
+	private static Logger logger = Logger.getLogger(NumeroPrimo.class.getName());
+	public static boolean esPrimo(int numero) {
+		logger.log(Level.INFO, "numero: " + numero);
+		for (int i = numero - 1; i > 0; i--) { 
+			System.out.println("Indice: "+ i + " modulo:" + (numero % i) );
+			if (numero % i == 0 && i != 1) { 
+				logger.log(Level.WARNING, "Divisor"+i + ",");
+				return false;
+			}	
+		}
+		return true;
+	}
+	
+}
